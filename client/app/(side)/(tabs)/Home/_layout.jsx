8fac7d410f888273503cd99e6a3fd4a75044aea3
Leah Deshar <leah.desar@gmail.com>
2024-07-02 07:20:22 +0545

    listing ml projects

--- client/app/(side)/(tabs)/Home/_layout.jsx
+++ client/app/(side)/(tabs)/Home/_layout.jsx
@@ -1,25 +1,70 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { useRouter } from "expo-router";
 import { useTheme } from "../../../../constants/ThemeProvider";
-import HousePricePredict from "../../../../components/HousePricePredict";
+
 const HomeScreen = () => {
   const { colors, setScheme, dark } = useTheme();
   const navigator = useRouter();
+
+  const projects = [
+    "Handwritten Digit Recognition",
+    "Image Classification with CIFAR-10",
+    "Face Detection",
+    "Object Detection with YOLO (You Only Look Once)",
+    "Emotion Detection from Facial Expressions",
+    "Dog Breed Classification",
+    "Plant Species Recognition",
+    "Traffic Sign Recognition",
+    "Food Classification",
+    "Bird Species Classification",
+    "Comment sentiment analyzer",
+  ];
+  const handlePress = (title) => {
+    Alert.alert("Project Selected", title);
+  };
+  return <ListProject projects={projects} handlePress={handlePress} />;
+};
+const ListProject = ({ projects, handlePress }) => {
   return (
-    <View style={{ flex: 1 }}>
-      <HousePricePredict />
+    <View style={styles.container}>
+      <FlatList
+        data={projects}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => handlePress(item)}
+            style={styles.item}
+          >
+            <Text style={styles.title}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.key}
+      />
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // justifyContent: "center",
-    // alignItems: "center",
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 20,
   },
-  text: {
+  item: {
+    backgroundColor: "#adddff",
+    padding: 20,
+    marginVertical: 8,
+    borderRadius: 10,
+  },
+  title: {
     fontSize: 18,
   },
 });
---
+++ client/app/sentiment/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SentimentScreen = () => {
+  return (
+    <View>
+      <Text>SentimentScreen</Text>
+    </View>
+  );
+};
+
+export default SentimentScreen;
+
+const styles = StyleSheet.create({});
